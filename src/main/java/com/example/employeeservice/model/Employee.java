17d8b88472920fb768d3e82c691a2d3d4da8117e
LittleArm <hien.lequang47@hcmut.edu.vn>
2024-07-11 16:18:30 +0700

    Update Employee Model to implement UserDetails

--- src/main/java/com/example/employeeservice/model/Employee.java
+++ src/main/java/com/example/employeeservice/model/Employee.java
@@ -3,27 +3,79 @@ package com.example.employeeservice.model;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.io.Serializable;
+import java.security.Principal;
 import java.time.LocalDate;
+import java.util.Collection;
+import java.util.Collections;
 
-@Entity
 @Getter
 @Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 @Table(name = "employee")
 @EntityListeners(AuditingEntityListener.class)
-public class Employee {
+public class Employee implements UserDetails, Principal, Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
     private Long id;
     private String firstName;
     private String lastName;
+    @Column(unique = true)
     private String email;
     private String jobTitle;
     private LocalDate dateOfBirth;
     private String imageUrl;
+
     @Column(nullable = false, updatable = false)
     private String employeeCode;
+
+    private Boolean locked = false;
+    private Boolean enabled = true;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !locked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    @Override
+    public String getName() {
+        return email;
+    }
 }
