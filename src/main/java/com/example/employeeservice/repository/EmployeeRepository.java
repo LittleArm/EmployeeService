02a09284da5ca1284ca135132f25c16e1f2edd7e
LittleArm <hien.lequang47@hcmut.edu.vn>
2024-07-11 16:17:48 +0700

    Add delete method

--- src/main/java/com/example/employeeservice/repository/EmployeeRepository.java
+++ src/main/java/com/example/employeeservice/repository/EmployeeRepository.java
@@ -6,5 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    void deleteEmployeeById(Long id);
+
     Optional<Employee> findByEmail(String email);
 }
