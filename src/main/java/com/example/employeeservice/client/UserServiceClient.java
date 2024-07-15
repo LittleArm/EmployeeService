33dd825ad8a1813855a3b8f61059fdc856566fb8
LittleArm <hien.lequang47@hcmut.edu.vn>
2024-07-15 17:39:40 +0700

    Update UserServiceClient.java

--- src/main/java/com/example/employeeservice/client/UserServiceClient.java
+++ src/main/java/com/example/employeeservice/client/UserServiceClient.java
@@ -1,8 +1,15 @@
 package com.example.employeeservice.client;
 
 import com.example.employeeservice.model.User;
+import com.example.employeeservice.security.JwtAuthenticationToken;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.*;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -17,21 +24,28 @@ public class UserServiceClient {
         this.restTemplate = restTemplate;
     }
 
-    public User getUserByEmail(String email, String jwtToken) {
-        String url = userServiceUrl + "/email?email=" + email;
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", "Bearer " + jwtToken);
-        HttpEntity<String> entity = new HttpEntity<>(headers);
-        ResponseEntity<User> response = restTemplate.exchange(
-                url,
-                HttpMethod.GET,
-                entity,
-                User.class
-        );
-        return response.getBody();
+    public User getUserByEmail(String email) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication instanceof JwtAuthenticationToken) {
+            String jwtToken = ((JwtAuthenticationToken) authentication).getToken();
+            String url = userServiceUrl + "/email?email=" + email;
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("Authorization", "Bearer " + jwtToken);
+            HttpEntity<String> entity = new HttpEntity<>(headers);
+            ResponseEntity<User> response = restTemplate.exchange(
+                    url,
+                    HttpMethod.GET,
+                    entity,
+                    User.class
+            );
+            return response.getBody();
+        }
+        throw new IllegalStateException("JWT token not found in security context");
     }
 
-    public void updateUser(User user, String jwtToken) {
+    public void updateUser(User user) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String jwtToken = ((JwtAuthenticationToken) authentication).getToken();
         String url = userServiceUrl + "/update";
         HttpHeaders headers = new HttpHeaders();
         headers.set("Authorization", "Bearer " + jwtToken);
