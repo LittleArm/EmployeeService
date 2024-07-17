b4579980608268ff8f02b11afc154a10a8fb5c00
LittleArm <hien.lequang47@hcmut.edu.vn>
2024-07-17 11:04:25 +0700

    Refactor the implementation of UserDetailService

---
+++ src/main/java/com/example/employeeservice/services/UserDetailServiceImpl.java
@@ -0,0 +1,24 @@
+package com.example.employeeservice.services;
+
+import com.example.employeeservice.repositories.EmployeeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailServiceImpl implements UserDetailsService {
+
+    private final EmployeeRepository repository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+        return repository.findByEmail(userEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found!"));
+    }
+}
+
