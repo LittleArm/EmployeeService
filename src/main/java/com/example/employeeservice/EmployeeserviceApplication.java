4ad9f96d68c6a4d61d9ab61c5a88913e292a08b8
LittleArm <hien.lequang47@hcmut.edu.vn>
2024-07-17 11:07:27 +0700

    Integrate swagger

--- src/main/java/com/example/employeeservice/EmployeeserviceApplication.java
+++ src/main/java/com/example/employeeservice/EmployeeserviceApplication.java
@@ -2,8 +2,12 @@ package com.example.employeeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableAsync
 public class EmployeeserviceApplication {
 
 	public static void main(String[] args) {
